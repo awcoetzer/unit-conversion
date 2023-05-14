03369116dcfabca08d828ddd005cb9ad66ceb1ce
awcoetzer <83278375+awcoetzer@users.noreply.github.com>
2023-05-14 16:54:42 +0300

    refactor: refactor the javascript code
    Refactored the javascript code.

--- resources/javascript/app.js
+++ resources/javascript/app.js
@@ -6,35 +6,24 @@ const resultHeadingEl = document.querySelectorAll('.result-heading');
 
 const resultTextsEl = document.querySelectorAll('.result-text');
 
+const lengthFormula = 3.281;
+const volumeFormula = 0.264;
+const massFormula = 2.204;
 
+const conversion = function (userValue, unitFormula, leftName, rightName) {
+  const leftConversion = userValue * unitFormula;
+  const rightConversion = userValue / unitFormula;
 
-const lengthCalc = function (userValue) {
-  const formulaValue = 3.281;
-  const toFeet = userValue * formulaValue;
-  const toMeter = userValue / formulaValue;
-
-  return `${userValue} meters = ${toFeet.toFixed(3)} feet | ${userValue} feet = ${toMeter.toFixed(3)} meters`
-}
-
-const volumeCalc = function (userValue) {
-  const formulaValue = 0.264;
-  const toGallons = userValue * formulaValue;
-  const toLiters = userValue / formulaValue;
-
-  return `${userValue} liters = ${toGallons.toFixed(3)} gallons | ${userValue} gallons = ${toLiters.toFixed(3)} liters`
-}
-
-const massCalc = function (userValue) {
-  const formulaValue = 2.204;
-  const toPounds = userValue * formulaValue;
-  const toKilos = userValue / formulaValue;
-
-  return `${userValue} kilos = ${toPounds.toFixed(3)} pounds | ${userValue} pounds = ${toKilos.toFixed(3)} kilos`
+  return `${userValue} ${leftName} = ${leftConversion.toFixed(3)} ${rightName} | ${userValue} ${rightName} = ${rightConversion.toFixed(3)} ${leftName}`
 }
 
 convertBtnEl.addEventListener('click', function () {
   let inputValue = Number(inputEl.value)
-  const results = [lengthCalc(inputValue), volumeCalc(inputValue), massCalc(inputValue)]
+  const results = [
+    conversion(inputValue, lengthFormula, 'meters', 'feet'),
+    conversion(inputValue, volumeFormula, 'liters', 'gallons'),
+    conversion(inputValue, massFormula, 'kilos', 'pounds')
+  ]
 
   for (let i = 0; i < resultTextsEl.length; i++) {
     resultTextsEl[i].textContent = results[i]

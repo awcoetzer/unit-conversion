df705bf46c38615a22dd6f5db17dbc102e66703f
awcoetzer <83278375+awcoetzer@users.noreply.github.com>
2023-05-14 18:11:31 +0300

    fix: add documentation
    Added comments for each thing in the file.

--- extension/resources/javascript/app-ext.js
+++ extension/resources/javascript/app-ext.js
@@ -2,14 +2,19 @@
 
 const inputEl = document.getElementById('input-el');
 const convertBtnEl = document.getElementById('convert-btn');
-const resultHeadingEl = document.querySelectorAll('.result-heading');
 
 const resultTextsEl = document.querySelectorAll('.result-text');
 
+// declaring the formula values
 const lengthFormula = 3.281;
 const volumeFormula = 0.264;
 const massFormula = 2.204;
 
+/*
+  one function to rule them all 🤣, this function 
+  takes in various parameters to help output
+  the desired outcome.
+*/
 const conversion = function (userValue, unitFormula, leftName, rightName) {
   const leftConversion = userValue * unitFormula;
   const rightConversion = userValue / unitFormula;
@@ -17,7 +22,12 @@ const conversion = function (userValue, unitFormula, leftName, rightName) {
   return `${userValue} ${leftName} = ${leftConversion.toFixed(3)} ${rightName} | ${userValue} ${rightName} = ${rightConversion.toFixed(3)} ${leftName}`
 }
 
-// the event listener button for conversion
+/* 
+  the event listener grabs the input value
+  and stores that in a variable, it also iterates
+  over the p elements and systematically places
+  the results of the result array for each p element.
+*/
 convertBtnEl.addEventListener('click', function () {
   let inputValue = Number(inputEl.value)
   const results = [

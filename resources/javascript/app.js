edf611c1c48aab894b0a98ab6fef01f6d603ede0
awcoetzer <83278375+awcoetzer@users.noreply.github.com>
2023-05-15 20:24:32 +0300

    feat: add expand & control of input field size
    Added restrictions on the input element, as well as
    allow it to expand and overflow on larger numbers

--- resources/javascript/app.js
+++ resources/javascript/app.js
@@ -5,10 +5,23 @@ const convertBtnEl = document.getElementById('convert-btn');
 
 const resultTextsEl = document.querySelectorAll('.result-text');
 
+let inputValue = Number(inputEl.value)
 // declaring the formula values
 const lengthFormula = 3.281;
 const volumeFormula = 0.264;
 const massFormula = 2.204;
+
+// to control the input length
+const expandInputField = function () {
+  let inputLength = inputEl.value.length
+  if (inputLength < 2) {
+    this.style.width =  '11.7rem' 
+  } else if (inputLength > 2 && inputLength < 7) {
+    this.style.width = ++inputLength + 'ch';
+  } else {
+    this.style.overflowX = 'scroll';
+  } 
+}
 
 /*
   one function to rule them all 🤣, this function 
@@ -22,6 +35,8 @@ const conversion = function (userValue, unitFormula, leftName, rightName) {
   return `${userValue} ${leftName} = ${leftConversion.toFixed(3)} ${rightName} | ${userValue} ${rightName} = ${rightConversion.toFixed(3)} ${leftName}`
 }
 
+inputEl.addEventListener('input', expandInputField)
+
 /* 
   the event listener grabs the input value
   and stores that in a variable, it also iterates
@@ -29,7 +44,8 @@ const conversion = function (userValue, unitFormula, leftName, rightName) {
   the results of the result array for each p element.
 */
 convertBtnEl.addEventListener('click', function () {
-  let inputValue = Number(inputEl.value)
+  inputValue = inputEl.value
+  console.log(typeof inputValue)
   const results = [
     conversion(inputValue, lengthFormula, 'meters', 'feet'),
     conversion(inputValue, volumeFormula, 'liters', 'gallons'),
